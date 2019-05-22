1f477f987ccbd427df0c7829d54b8cc07b66a5a5
Devilisch <kirowatanabe@gmail.com>
2019-05-22 21:15:40 +0300

    hot fix

--- src/gui.lua
+++ src/gui.lua
@@ -1,20 +1,18 @@
 require('core')
 gui = {
-  command = {'m', -1, -1, ''},
-  status = 's'
+  command = {'s', -1, -1, ''}
 }
 
 function gui.readCommand()
   io.write('Enter command: ')
   gui.command[1] = io.read(1)
+  --io.write(gui.command[1],'\n')
   if gui.command[1] == 'm' then
-    gui.command[2] = io.read("*number")
-    gui.command[3] = io.read("*number")
-    io.read(1)
-    gui.command[4] = io.read(1)
+    gui.command[2], gui.command[3], gui.command[4] = io.read("*number", "*number", 1)
+    io.write('Input command: ', gui.command[1], ' ', gui.command[2], ' ', gui.command[3], ' ', gui.command[4],'\n')
     core.move({gui.command[2], gui.command[3]},gui.command[4])
+
   end
   io.read(1)
-  gui.status = gui.command[1]
-  io.write('Input command ', gui.command[1], ' ', gui.command[2], ' ', gui.command[3], ' ', gui.command[4],'\n')
+  io.write('\n')
 end
--- src/main.lua
+++ src/main.lua
@@ -2,24 +2,39 @@ require('gui')
 
 
 
+
+
 local function main()
 -- инициализация интерфейса
-  start = 0
-  core.init()
-  --core.dump()
+  
+	start = 0
+  
+	core.init()
 -- основной цикл
-  while gui.status ~= 'q' do
-    if start > 0 then gui.readCommand() end
-    repeat
-      core.tick()
-      core.init()
-    until core.flag < 2
-    if start == 0 then core.score = 0 end
-    core.dump()
-    core.mix()
-    start = 1
-  end
-
+  
+	while gui.command[1] ~= 'q' do
+    
+		if start > 0 then gui.readCommand() end
+    
+		repeat
+      
+		core.tick()
+      
+		core.init()
+    
+		until core.flag < 2
+    
+		if start == 0 then core.score = 0 end
+    
+		core.dump()    
+    
+		core.mix()
+    
+		start = 1
+  
+	end
 end
+
+
 
 main()
